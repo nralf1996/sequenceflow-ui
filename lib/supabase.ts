48147416b398f3d3e49c4164f978c1089d3fb5ae
Navaron Ralf <navaronralf@mac.home>
2026-02-27 12:21:47 +0100

    fix: supabase env typing + dependency update

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,14 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.SUPABASE_URL!;
-const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
+const supabaseUrl = process.env.SUPABASE_URL as string;
+const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY as string;
+
+if (!supabaseUrl) {
+  throw new Error("Missing SUPABASE_URL");
+}
+
+if (!supabaseKey) {
+  throw new Error("Missing SUPABASE_SERVICE_ROLE_KEY");
+}
 
 export const supabase = createClient(supabaseUrl, supabaseKey);

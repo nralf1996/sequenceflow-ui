68214d06b74511e4f37fd8d06e61b1cb63355fdd
SequenceFlowNL <sequenceflownl@gmail.com>
2026-03-13 12:57:38 +0100

    Update layout.tsx

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -2,6 +2,9 @@ import { ThemeProvider } from "@/lib/theme/ThemeProvider";
 import { LanguageProvider } from "@/lib/i18n/LanguageProvider";
 import { AppShell } from "@/components/AppShell";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export default function AppLayout({
   children,
 }: {
